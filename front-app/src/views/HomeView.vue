de9a36f7b030099813ed0eb5bd0012e9589e58e4
sasoriii <44494389+sasoriii@users.noreply.github.com>
2022-06-28 11:04:51 +0200

    home

--- front-app/src/views/HomeView.vue
+++ front-app/src/views/HomeView.vue
@@ -1,15 +1,59 @@
+<style scoped>
+.carrousel {
+  display: block;
+  margin-top: 100px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 60%;
+}
+</style>
+
+
 <template>
-  <hello-world />
+<v-carousel
+  class="carrousel"
+  cycle
+  height="400"
+  hide-delimiter-background
+  show-arrows-on-hover
+>
+  <v-carousel-item
+  v-for="(item,i) in items"
+  :key="i"
+  :src="item.src"
+  
+  ></v-carousel-item>
+</v-carousel>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  
 
   export default {
     name: 'HomeView',
-
+    data () {
+      return {
+        items: [
+          {
+            src: require('@/assets/gros-bao.png '),
+          },
+          {
+            src: require('@/assets/1745.png'),
+          },
+          {
+            src: require('@/assets/tonton.png'),
+          },
+          {
+            src: require('@/assets/levain.png'),
+          },
+          {
+            src: require('@/assets/tacos.png'),
+          },
+        ],
+      }
+    },
     components: {
-      HelloWorld,
+      
     },
   }
 </script>
