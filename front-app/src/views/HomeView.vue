32e65e8cbe5977d58a9e9c82d38b496b6ebcd3f7
Jackalco <vincent.jacques@viacesi.fr>
2022-06-29 15:14:21 +0200

    Home done, just need to add the link of the restaurant inside the button

--- front-app/src/views/HomeView.vue
+++ front-app/src/views/HomeView.vue
@@ -4,16 +4,34 @@
   margin-top: 100px;
   margin-left: auto;
   margin-right: auto;
-  width: 60%;
+  width: 100%;
 }
+
+.carrousel-container {
+  width: 100%;
+}
+
+.carrousel-button {
+  position: absolute;
+  left: 20%;
+  bottom: 20%;
+  font-size: 20px !important;
+  padding: 1.5em !important;
+}
+
+.carrousel-button:hover {
+
+}
+
 </style>
 
 
 <template>
-<v-carousel
+<div>
+  <v-carousel
   class="carrousel"
   cycle
-  height="400"
+  height="800"
   hide-delimiter-background
   show-arrows-on-hover
 >
@@ -23,7 +41,12 @@
   :src="item.src"
   
   ></v-carousel-item>
-</v-carousel>
+  
+  </v-carousel>
+  <v-btn elevation="5" color="primary" class="carrousel-button" href="">Découvrez nos restaurants</v-btn>
+</div>
+
+
 </template>
 
 <script>
@@ -35,7 +58,7 @@
       return {
         items: [
           {
-            src: require('@/assets/gros-bao.png '),
+            src: require('@/assets/gros-bao.png'),
           },
           {
             src: require('@/assets/1745.png'),
