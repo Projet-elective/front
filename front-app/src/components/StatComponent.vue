2c40516e98176e7d4771e4d8a1195d7dba7d27c8
Jackalco <vincent.jacques@viacesi.fr>
2022-06-30 13:30:22 +0200

    Add css for stat page

--- front-app/src/components/StatComponent.vue
+++ front-app/src/components/StatComponent.vue
@@ -1,52 +1,116 @@
+<style>
+.stat-body{
+    background-color: var(--v-secondary-base);
+    height: 100%;
+}
+
+.stat-container {
+    margin-right: 10%;
+    margin-left: 10%;
+    padding-left: 0;
+    padding-right: 0;
+    padding-top: 0;
+    width: auto;
+    background-color: white;
+    height: 100%;
+    box-shadow: 0px 6px 6px -3px rgb(0 0 0 / 20%), 0px 10px 14px 1px rgb(0 0 0 / 14%), 0px 4px 18px 3px rgb(0 0 0 / 12%) !important;
+}
+
+.container-title {
+    width: 100%;
+    height: 150px;
+    background-color: var(--v-accent-base);
+    padding-left: 10%;
+    padding-top: 50px;
+}
+
+.container-title h1 {
+    color: white;
+}
+
+.container-lists {
+    padding-right: 10%;
+    padding-left: 10%;
+}
+
+h2 {
+    background-color: green;
+    padding: 10px 30px;
+    width: fit-content;
+    color: white;
+    border-radius: 25px;
+    margin-bottom: 25px;
+    margin-top: 25px;
+}
+</style>
 <template>
-   <v-container>
+<div class="stat-body">
+    <v-container class="stat-container">
         <template v-if="this.tokenRole == 'COMMERCIAL'">
-            <template>
-                <h1 style="text-align: center;">Statistique commercials</h1>
-                <p>Commande de la semaine</p>
-                <ul v-for="week in weeks" :key="week">
-                    <li>{{week}}</li>   
-                </ul>
-            </template>
-                <v-spacer></v-spacer>
-            <template>
-                <p>Commande du mois</p>
-                <ul v-for="month in months" :key="month">
-                    <li>{{month}}</li>   
-                </ul>
-            </template>
-                <v-spacer></v-spacer>
-            <template>
-                <p>Commande de l'année</p>
-                <ul v-for="year in years" :key="year">
-                    <li>{{year}} </li>   
-                </ul>
-            </template>
-        <template>
+            <div class="container-title">
+                <h1>Statistique Commercial</h1>
+            </div>
+            <div class="container-lists">
+                <template>
+                    <h2>Commande de la semaine</h2>
+                    <ul v-for="week in weeks" :key="week">
+                        <li>{{week}}</li> 
+                        
+                    </ul>
+                    <ul>
+                        <li>text</li>  
+                        <li>text</li> 
+                        <li>text</li> 
+                    </ul>
+                </template>
+                    <v-spacer></v-spacer>
+                <template>
+                    <h2>Commande du mois</h2>
+                    <ul v-for="month in months" :key="month">
+                        <li>{{month}}</li>   
+                    </ul>
+                </template>
+                    <v-spacer></v-spacer>
+                <template>
+                    <h2>Commande de l'année</h2>
+                    <ul v-for="year in years" :key="year">
+                        <li>{{year}} </li>   
+                    </ul>
+                </template>
+            </div>
+            
         </template>
-            <template v-if="this.tokenRole == 'RESTAURANT'">
-                <h1 style="text-align: center;">Statistique restaurant</h1>
-                <p>Commande de la semaine</p>
-                <ul v-for="week in weeks" :key="week">
-                    <li>{{week}}</li>   
-                </ul>
-            </template>
-                <v-spacer></v-spacer>
-            <template>
-                <p>Commande du mois</p>
-                <ul v-for="month in months" :key="month">
-                    <li>{{month}}</li>   
-                </ul>
-            </template>
-                <v-spacer></v-spacer>
-            <template>
-                <p>Commande de l'année</p>
-                <ul v-for="year in years" :key="year">
-                    <li>{{year}} </li>   
-                </ul>
-            </template>
+        <template v-if="this.tokenRole == 'RESTAURANT'">
+                <div class="container-title">
+                    <h1>Statistique Restaurant</h1>
+                </div>
+                <div class="container-lists">
+                    <template>
+                        <h2>Commande de la semaine</h2>
+                        <ul v-for="week in weeks" :key="week">
+                            <li>{{week}}</li>   
+                        </ul>
+                    </template>
+                        <v-spacer></v-spacer>
+                    <template>
+                        <h2>Commande du mois</h2>
+                        <ul v-for="month in months" :key="month">
+                            <li>{{month}}</li>   
+                        </ul>
+                    </template>
+                        <v-spacer></v-spacer>
+                    <template>
+                        <h2>Commande de l'année</h2>
+                        <ul v-for="year in years" :key="year">
+                            <li>{{year}} </li>   
+                        </ul>
+                    </template>
+                </div>
+            
         </template>
-   </v-container> 
+   </v-container>
+</div>
+    
 </template>
 
 <script>
